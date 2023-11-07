f06ae9ea61bb52c92e6efed28313b454611665b7
mattgills <matt.gillespie24@gmail.com>
2023-11-06 21:22:48 -0500

    adds evaluation for booleans and adds null

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,20 @@ import (
 	"monkey/object"
 )
 
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+
+	return FALSE
+}
+
 func evalStatements(stmts []ast.Statement) object.Object {
 	var result object.Object
 
@@ -28,6 +42,10 @@ func Eval(node ast.Node) object.Object {
 	// Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
+
 	}
 
 	return nil
