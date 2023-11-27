ece0582a143b1e5695fc32a8c42e97e5d45bbda6
mattgills <matt.gillespie24@gmail.com>
2023-11-26 22:14:07 -0500

    Adds test for evaluating let statements

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -243,3 +243,19 @@ func TestError(t *testing.T) {
 		}
 	}
 }
+
+func TestLetStatements(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let a = 5; a;", 5},
+		{"let a = 5 * 5; a;", 25},
+		{"let a = 5; let b = a; b;", 5},
+		{"let a = 5; let b = a; let c = a + b + 5; c;", 15},
+	}
+
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
+	}
+}
