590614d121c3f0c24934da7aebff6d5d29207752
mattgills <matt.gillespie24@gmail.com>
2024-01-17 22:55:42 -0500

    adds macro expansion

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -947,33 +947,28 @@ func TestMacroLiteralParsing(t *testing.T) {
 
 	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
 	if !ok {
-		t.Fatalf("statement is not ast.ExpressionStatement. got=%T",
-			program.Statements[0])
+		t.Fatalf("statement is not ast.ExpressionStatement. got=%T", program.Statements[0])
 	}
 
 	macro, ok := stmt.Expression.(*ast.MacroLiteral)
 	if !ok {
-		t.Fatalf("stmt.Expression is not ast.MacroLiteral. got=%T",
-			stmt.Expression)
+		t.Fatalf("stmt.Expression is not ast.MacroLiteral. got=%T", stmt.Expression)
 	}
 
 	if len(macro.Parameters) != 2 {
-		t.Fatalf("macro literal parameters wrong. want 2, got=%d\n",
-			len(macro.Parameters))
+		t.Fatalf("macro literal parameters wrong. want 2, got=%d\n", len(macro.Parameters))
 	}
 
 	testLiteralExpression(t, macro.Parameters[0], "x")
 	testLiteralExpression(t, macro.Parameters[1], "y")
 
 	if len(macro.Body.Statements) != 1 {
-		t.Fatalf("macro.Body.Statements has not 1 statements. got=%d\n",
-			len(macro.Body.Statements))
+		t.Fatalf("macro.Body.Statements has not 1 statements. got=%d\n", len(macro.Body.Statements))
 	}
 
 	bodyStmt, ok := macro.Body.Statements[0].(*ast.ExpressionStatement)
 	if !ok {
-		t.Fatalf("macro body stmt is not ast.ExpressionStatement. got=%T",
-			macro.Body.Statements[0])
+		t.Fatalf("macro body stmt is not ast.ExpressionStatement. got=%T", macro.Body.Statements[0])
 	}
 
 	testInfixExpression(t, bodyStmt.Expression, "x", "+", "y")
